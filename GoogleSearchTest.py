258f8f073402892b52a8389db776caf45be146a3
sergiokneri <sergioneri@gmail.com>
2017-10-02 13:34:21 -0700

    Update GoogleSearchTest.py

--- GoogleSearchTest.py
+++ GoogleSearchTest.py
@@ -1,25 +1,23 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class GoogleSearchTest(unittest.TestCase):
 
+class TestGoogleSearchTest:
+    driver = None
 
-    def setUp(self):
+    def setup(self):
         # Create a new Firefox driver instance
         self.driver = webdriver.Firefox()
 
-    def tearDown(self):
+    def teardown(self):
         # Close the browser after running the tests
         self.driver.quit()
 
-    def testSearch(self):
+    def test_search(self):
         # Trigger a Google Search
         self.driver.get('http://www.google.com')
-        self.assertEqual(self.driver.title, 'Google')
         searchElement = self.driver.find_element_by_name('q')
         searchElement.send_keys('6020 peaks')
         searchElement.submit()
         WebDriverWait(self.driver, 10).until(EC.title_contains('6020 peaks'))
-        self.assertTrue(self.driver.title.startswith('6020 peaks'))
